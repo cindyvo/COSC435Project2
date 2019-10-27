5d26032a726f1fa01e210ab706dd6f63f3b0e899
cvo <cvo@colgate.edu>
2019-10-27 18:31:00 -0400

    adding starting code

---
+++ objects.js
@@ -0,0 +1,16 @@
+function radians(degrees) {
+  return degrees * Math.PI / 180.0;
+
+}
+
+function triangle(r, g, b, a) {
+  filledTriangle();
+}
+
+function circle(r, g, b,a ) {
+    filledCircle();
+}
+
+function square(colorArr) {
+    filledSquare(colorArr);
+}
